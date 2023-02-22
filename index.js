c0aba9f5657414ebf1d91d465cc849dd1bd030fa
Mohamed Aboubakr <mabobakr365@gmail.com>
2023-02-22 20:53:06 +0200

    Added all source code

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const allSearchOptions = Array.from(
   document.querySelectorAll(".search-option")
 );
 
-const allProductsArr = [];
+let allProductsArr = [];
 let productsCounter = 0;
 
 function calculateTotalPrice() {
@@ -43,9 +43,9 @@ function clearProductInputs() {
 function interactWithLocalStorage(interactMethod) {
   const interactMethodLower = interactMethod.toLowerCase();
   if (interactMethodLower === "set")
-    localStorage.setItem("allProducts", JSON.stringify(allProductsArr));
+    return localStorage.setItem("allProducts", JSON.stringify(allProductsArr));
   if (interactMethodLower === "get")
-    JSON.parse(localStorage.getItem("allProducts"));
+    return JSON.parse(localStorage.getItem("allProducts"));
 }
 
 function createNewProduct(fromLocalStorage = false, productInfoArr) {
@@ -126,6 +126,8 @@ function createNewProduct(fromLocalStorage = false, productInfoArr) {
         taxes: productTaxes !== "" ? productTaxes : "0",
         ads: productAds !== "" ? productAds : "0",
         discount: productDiscount !== "" ? productDiscount : "0",
+        // count: productCount,
+        category: productCategory,
         totalPrice: totalPriceSpot.textContent,
       });
   }
@@ -156,6 +158,12 @@ function searchForProduct(searchOption) {
       case "normal":
         product.dataset.visibility = "visible";
         break;
+      case "id":
+        const id = product.querySelector(".id").textContent;
+        id.startsWith(searchQuery)
+          ? (product.dataset.visibility = "visible")
+          : (product.dataset.visibility = "hidden");
+        break;
       case "title":
         const title = product.querySelector(".title").textContent;
         title.includes(searchQuery)
@@ -164,31 +172,31 @@ function searchForProduct(searchOption) {
         break;
       case "price":
         const price = product.querySelector(".price").textContent;
-        price === searchQuery
+        price.startsWith(searchQuery)
           ? (product.dataset.visibility = "visible")
           : (product.dataset.visibility = "hidden");
         break;
       case "taxes":
         const taxes = product.querySelector(".taxes").textContent;
-        taxes === searchQuery
+        taxes.startsWith(searchQuery)
           ? (product.dataset.visibility = "visible")
           : (product.dataset.visibility = "hidden");
         break;
       case "ads":
         const ads = product.querySelector(".ads").textContent;
-        ads === searchQuery
+        ads.startsWith(searchQuery)
           ? (product.dataset.visibility = "visible")
           : (product.dataset.visibility = "hidden");
         break;
       case "discount":
         const discount = product.querySelector(".discount").textContent;
-        discount === searchQuery
+        discount.startsWith(searchQuery)
           ? (product.dataset.visibility = "visible")
           : (product.dataset.visibility = "hidden");
         break;
       case "total":
         const total = product.querySelector(".total").textContent;
-        total === searchQuery
+        total.startsWith(searchQuery)
           ? (product.dataset.visibility = "visible")
           : (product.dataset.visibility = "hidden");
         break;
@@ -200,6 +208,62 @@ function searchForProduct(searchOption) {
         break;
     }
   });
+}
+
+function initEverthingBack() {
+  const allProductsArrFromLocalStorage = interactWithLocalStorage("Get");
+  if (allProductsArrFromLocalStorage == null) return;
+  allProductsArr = allProductsArrFromLocalStorage;
+  allProductsArr.forEach((product) => {
+    const newProductElement = `
+    <tr>
+      <td class="id">${product.ID}</td>
+      <td class="title">${product.name}</td>
+      <td class="price">${product.price}</td>
+      <td class="taxes">${product.taxes}</td>
+      <td class="ads">${product.ads}</td>
+      <td class="discount">${product.discount}</td>
+      <td class="category">${product.category}</td>
+      <td class="total">${product.totalPrice}</td>
+      <td class="manipulate-product-spot">
+          <button class="btn-edit-product" title="Edit the product">
+              <svg
+                  fill="#fff"
+                  width="25px"
+                  height="25px"
+                  viewBox="0 0 24 24"
+                  xmlns="http://www.w3.org/2000/svg"
+              >
+                  <path
+                  d="M18.111,2.293,9.384,11.021a.977.977,0,0,0-.241.39L8.052,14.684A1,1,0,0,0,9,16a.987.987,0,0,0,.316-.052l3.273-1.091a.977.977,0,0,0,.39-.241l8.728-8.727a1,1,0,0,0,0-1.414L19.525,2.293A1,1,0,0,0,18.111,2.293ZM11.732,13.035l-1.151.384.384-1.151L16.637,6.6l.767.767Zm7.854-7.853-.768.767-.767-.767.767-.768ZM3,5h8a1,1,0,0,1,0,2H4V20H17V13a1,1,0,0,1,2,0v8a1,1,0,0,1-1,1H3a1,1,0,0,1-1-1V6A1,1,0,0,1,3,5Z"
+                  />
+              </svg>
+              Edit
+          </button>
+      </td>
+      <td class="manipulate-product-spot">
+          <button class="btn-delete-product" title="Delete the product">
+              <svg
+                  width="25px"
+                  height="25px"
+                  viewBox="0 0 1024 1024"
+                  xmlns="http://www.w3.org/2000/svg"
+              >
+                  <path
+                  fill="#fff"
+                  d="M160 256H96a32 32 0 0 1 0-64h256V95.936a32 32 0 0 1 32-32h256a32 32 0 0 1 32 32V192h256a32 32 0 1 1 0 64h-64v672a32 32 0 0 1-32 32H192a32 32 0 0 1-32-32V256zm448-64v-64H416v64h192zM224 896h576V256H224v640zm192-128a32 32 0 0 1-32-32V416a32 32 0 0 1 64 0v320a32 32 0 0 1-32 32zm192 0a32 32 0 0 1-32-32V416a32 32 0 0 1 64 0v320a32 32 0 0 1-32 32z"
+                  />
+              </svg>
+              Delete
+          </button>
+      </td>
+  </tr>
+    `;
+    tableBody.insertAdjacentHTML("beforeend", newProductElement);
+  });
+
+  productsCounter = allProductsArrFromLocalStorage.length;
+  allProductsNum.textContent = `(${productsCounter})`;
 }
 
 ["click", "input"].forEach((event) => {
@@ -228,6 +292,8 @@ searchOptionsSpot.addEventListener("click", (event) => {
   if (clickedSpot == null) return;
   allSearchOptions.forEach((option) => (option.dataset.state = "unchecked"));
   clickedSpot.dataset.state = "checked";
+  if (searchInput.value !== "")
+    searchForProduct(clickedSpot.querySelector("input").value);
 });
 
 searchInput.addEventListener("input", () => {
@@ -235,7 +301,7 @@ searchInput.addEventListener("input", () => {
     (option) => option.dataset.state === "checked"
   );
   if (searchInput.value === "") return searchForProduct("normal");
-  if (checkedRadioParent == null && searchInput.value !== "")
-    return showError("Check any search option at first!");
   searchForProduct(checkedRadioParent.querySelector("input").value);
 });
+
+window.addEventListener("load", initEverthingBack);
