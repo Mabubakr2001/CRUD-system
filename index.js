35939354b23374b143e233ea446560d2081912e2
Mohamed Aboubakr <mabobakr365@gmail.com>
2023-02-24 21:13:30 +0200

    Added all source code

--- index.js
+++ index.js
@@ -9,13 +9,12 @@ const categoryInput = document.querySelector(".input-category");
 const btnCreateProduct = document.querySelector(".btn-create-product");
 const btnUpdateProduct = document.querySelector(".btn-update-product");
 const tableBody = document.querySelector(".products-table-body");
-const allProductsNum = document.querySelector(".all-products-num");
 const searchInput = document.querySelector(".search-input");
-const searchBtn = document.querySelector(".search-btn");
 const searchOptionsSpot = document.querySelector(".search-options");
 const allSearchOptions = Array.from(
   document.querySelectorAll(".search-option")
 );
+const allProductsNum = document.querySelector(".all-products-num");
 const btnEditProduct = document.querySelector(".btn-edit-product");
 const btnDeleteProduct = document.querySelector(".btn-delete-product");
 const btnDeleteAllProducts = document.querySelector(".delete-all-btn");
@@ -59,7 +58,7 @@ function interactWithLocalStorage(interactMethod) {
     return JSON.parse(localStorage.getItem("allProducts"));
 }
 
-function generateProductMarkup(productPropertiesArr) {
+function createNewProduct(fromLocalStorage = undefined, ...productProperties) {
   const [
     productID,
     productName,
@@ -67,117 +66,79 @@ function generateProductMarkup(productPropertiesArr) {
     productTaxes,
     productAds,
     productDiscount,
-    productCategory,
-    totalPrice,
-  ] = productPropertiesArr;
-
-  const markup = `
-    <tr>
-      <td class="id">${productID}</td>
-      <td class="title">${productName}</td>
-      <td class="price">${productPrice}</td>
-      <td class="taxes">${productTaxes !== "" ? productTaxes : "0"}</td>
-      <td class="ads">${productAds !== "" ? productAds : "0"}</td>
-      <td class="discount">${
-        productDiscount !== "" ? productDiscount : "0"
-      }</td>
-      <td class="category">${
-        productCategory !== "" ? productCategory : "No category"
-      }</td>
-      <td class="total">${totalPrice}</td>
-      <td class="manipulate-product-spot">
-          <button class="btn-edit-product" title="Edit the product">
-              <svg
-                  fill="#fff"
-                  width="25px"
-                  height="25px"
-                  viewBox="0 0 24 24"
-                  xmlns="http://www.w3.org/2000/svg"
-              >
-                  <path
-                  d="M18.111,2.293,9.384,11.021a.977.977,0,0,0-.241.39L8.052,14.684A1,1,0,0,0,9,16a.987.987,0,0,0,.316-.052l3.273-1.091a.977.977,0,0,0,.39-.241l8.728-8.727a1,1,0,0,0,0-1.414L19.525,2.293A1,1,0,0,0,18.111,2.293ZM11.732,13.035l-1.151.384.384-1.151L16.637,6.6l.767.767Zm7.854-7.853-.768.767-.767-.767.767-.768ZM3,5h8a1,1,0,0,1,0,2H4V20H17V13a1,1,0,0,1,2,0v8a1,1,0,0,1-1,1H3a1,1,0,0,1-1-1V6A1,1,0,0,1,3,5Z"
-                  />
-              </svg>
-              Edit
-          </button>
-      </td>
-      <td class="manipulate-product-spot">
-          <button class="btn-delete-product" title="Delete the product">
-              <svg
-                  width="25px"
-                  height="25px"
-                  viewBox="0 0 1024 1024"
-                  xmlns="http://www.w3.org/2000/svg"
-              >
-                  <path
-                  fill="#fff"
-                  d="M160 256H96a32 32 0 0 1 0-64h256V95.936a32 32 0 0 1 32-32h256a32 32 0 0 1 32 32V192h256a32 32 0 1 1 0 64h-64v672a32 32 0 0 1-32 32H192a32 32 0 0 1-32-32V256zm448-64v-64H416v64h192zM224 896h576V256H224v640zm192-128a32 32 0 0 1-32-32V416a32 32 0 0 1 64 0v320a32 32 0 0 1-32 32zm192 0a32 32 0 0 1-32-32V416a32 32 0 0 1 64 0v320a32 32 0 0 1-32 32z"
-                  />
-              </svg>
-              Delete
-          </button>
-      </td>
-  </tr>
-    `;
-  tableBody.insertAdjacentHTML("beforeend", markup);
-}
-
-function createNewProduct(productInfoArr) {
-  const [
-    productName,
-    productPrice,
-    productTaxes,
-    productAds,
-    productDiscount,
+    productTotalPrice,
     productCount,
     productCategory,
-  ] = productInfoArr;
-
-  if (productPrice === "" || +productPrice === 0)
-    return showError(
-      "Please fill out all required fields (Title, Price, Count)!"
-    );
-  if (productCount === "" || +productCount === 0)
-    return showError(
-      "Please fill out all required fields (Title, Price, Count)!"
-    );
-  if (productName === "")
-    return showError(
-      "Please fill out all required fields (Title, Price, Count)!"
-    );
+  ] = productProperties;
 
   productsCounter += +productCount;
   allProductsNum.textContent = `(${productsCounter})`;
 
-  for (let i = 0; i < +productCount; i++) {
-    const randomProductID = Math.floor(Math.random() * 100000000);
-    generateProductMarkup([
-      randomProductID,
-      productName,
-      productPrice,
-      productTaxes,
-      productAds,
-      productDiscount,
-      productCategory,
-      totalPriceSpot.textContent,
-    ]);
+  const markup = `
+  <tr>
+    <td class="id">${productID}</td>
+    <td class="title">${productName}</td>
+    <td class="price">${productPrice}</td>
+    <td class="taxes">${productTaxes !== "" ? productTaxes : "0"}</td>
+    <td class="ads">${productAds !== "" ? productAds : "0"}</td>
+    <td class="discount">${productDiscount !== "" ? productDiscount : "0"}</td>
+    <td class="count">${productCount}</td>
+    <td class="category">${
+      productCategory !== "" ? productCategory : "No category"
+    }</td>
+    <td class="total">${productTotalPrice}</td>
+    <td class="manipulate-product-spot">
+        <button class="btn-edit-product" title="Edit the product">
+            <svg
+                fill="#fff"
+                width="25px"
+                height="25px"
+                viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg"
+            >
+                <path
+                d="M18.111,2.293,9.384,11.021a.977.977,0,0,0-.241.39L8.052,14.684A1,1,0,0,0,9,16a.987.987,0,0,0,.316-.052l3.273-1.091a.977.977,0,0,0,.39-.241l8.728-8.727a1,1,0,0,0,0-1.414L19.525,2.293A1,1,0,0,0,18.111,2.293ZM11.732,13.035l-1.151.384.384-1.151L16.637,6.6l.767.767Zm7.854-7.853-.768.767-.767-.767.767-.768ZM3,5h8a1,1,0,0,1,0,2H4V20H17V13a1,1,0,0,1,2,0v8a1,1,0,0,1-1,1H3a1,1,0,0,1-1-1V6A1,1,0,0,1,3,5Z"
+                />
+            </svg>
+            Edit
+        </button>
+    </td>
+    <td class="manipulate-product-spot">
+        <button class="btn-delete-product" title="Delete the product">
+            <svg
+                width="25px"
+                height="25px"
+                viewBox="0 0 1024 1024"
+                xmlns="http://www.w3.org/2000/svg"
+            >
+                <path
+                fill="#fff"
+                d="M160 256H96a32 32 0 0 1 0-64h256V95.936a32 32 0 0 1 32-32h256a32 32 0 0 1 32 32V192h256a32 32 0 1 1 0 64h-64v672a32 32 0 0 1-32 32H192a32 32 0 0 1-32-32V256zm448-64v-64H416v64h192zM224 896h576V256H224v640zm192-128a32 32 0 0 1-32-32V416a32 32 0 0 1 64 0v320a32 32 0 0 1-32 32zm192 0a32 32 0 0 1-32-32V416a32 32 0 0 1 64 0v320a32 32 0 0 1-32 32z"
+                />
+            </svg>
+            Delete
+        </button>
+    </td>
+</tr>
+  `;
+  tableBody.insertAdjacentHTML("beforeend", markup);
 
+  if (!fromLocalStorage) {
     allProductsArr.push({
-      ID: randomProductID.toString(),
+      ID: productID,
       name: productName,
       price: productPrice,
       taxes: productTaxes !== "" ? productTaxes : "0",
       ads: productAds !== "" ? productAds : "0",
       discount: productDiscount !== "" ? productDiscount : "0",
+      count: productCount,
       category: productCategory,
-      totalPrice: totalPriceSpot.textContent,
+      totalPrice: productTotalPrice,
     });
+    interactWithLocalStorage("Set");
+    clearProductInputs();
+    totalPriceSpot.textContent = "";
   }
-
-  interactWithLocalStorage("Set");
-
-  clearProductInputs();
-  totalPriceSpot.textContent = "";
 }
 
 function showError(errorMessage) {
@@ -242,6 +203,12 @@ function searchForProduct(searchOption) {
           ? (product.dataset.state = "visible")
           : (product.dataset.state = "hidden");
         break;
+      case "count":
+        const count = product.querySelector(".count").textContent;
+        count.startsWith(searchQuery)
+          ? (product.dataset.state = "visible")
+          : (product.dataset.state = "hidden");
+        break;
       case "category":
         const category = product.querySelector(".category").textContent;
         category.includes(searchQuery)
@@ -250,18 +217,6 @@ function searchForProduct(searchOption) {
         break;
     }
   });
-}
-
-function manipulateDeletionView(state) {
-  const stateLower = state.toLowerCase();
-  if (stateLower === "show") {
-    deletionWindow.dataset.state = "visible";
-    overlay.dataset.state = "visible";
-  }
-  if (stateLower === "hide") {
-    deletionWindow.dataset.state = "hidden";
-    overlay.dataset.state = "hidden";
-  }
 }
 
 function editTheProduct(productID) {
@@ -273,11 +228,7 @@ function editTheProduct(productID) {
     (actualProduct) => actualProduct.ID === productID
   );
 
-  if (targetProductElement == null) return;
-  if (titleInput.value === "")
-    return showError("Please fill out all required fields (Title, Price)!");
-  if (priceInput.value === "" || +priceInput.value === 0)
-    return showError("Please fill out all required fields (Title, Price)!");
+  if (targetProductElement == null || targetActualProduct == null) return;
 
   targetProductElement.querySelector(".title").textContent = titleInput.value;
   targetProductElement.querySelector(".price").textContent = priceInput.value;
@@ -287,10 +238,11 @@ function editTheProduct(productID) {
     adsInput.value !== "" ? adsInput.value : "0";
   targetProductElement.querySelector(".discount").textContent =
     discountInput.value !== "" ? discountInput.value : "0";
-  targetProductElement.querySelector(".category").textContent =
-    categoryInput.value !== "" ? categoryInput.value : "No category";
   targetProductElement.querySelector(".total").textContent =
     totalPriceSpot.textContent;
+  targetProductElement.querySelector(".count").textContent = countInput.value;
+  targetProductElement.querySelector(".category").textContent =
+    categoryInput.value !== "" ? categoryInput.value : "No category";
 
   targetActualProduct.name = titleInput.value;
   targetActualProduct.price = priceInput.value;
@@ -298,16 +250,16 @@ function editTheProduct(productID) {
   targetActualProduct.ads = adsInput.value !== "" ? adsInput.value : "0";
   targetActualProduct.discount =
     discountInput.value !== "" ? discountInput.value : "0";
+  targetActualProduct.totalPrice = totalPriceSpot.textContent;
+  targetActualProduct.count = countInput.value;
   targetActualProduct.category =
     categoryInput.value !== "" ? categoryInput.value : "No category";
-  targetActualProduct.totalPrice = totalPriceSpot.textContent;
 
   interactWithLocalStorage("Set");
   clearProductInputs();
 
   btnCreateProduct.dataset.state = "visible";
   btnUpdateProduct.dataset.state = "hidden";
-  countInput.disabled = false;
 }
 
 function deleteTheProduct(productID) {
@@ -344,6 +296,18 @@ function deleteAllProducts() {
   allProductsNum.textContent = `(${productsCounter})`;
 }
 
+function manipulateDeletionView(state) {
+  const stateLower = state.toLowerCase();
+  if (stateLower === "show") {
+    deletionWindow.dataset.state = "visible";
+    overlay.dataset.state = "visible";
+  }
+  if (stateLower === "hide") {
+    deletionWindow.dataset.state = "hidden";
+    overlay.dataset.state = "hidden";
+  }
+}
+
 function initEverthingBack() {
   const allProductsArrFromLocalStorage = interactWithLocalStorage("Get");
 
@@ -356,16 +320,18 @@ function initEverthingBack() {
   allProductsArr = allProductsArrFromLocalStorage;
 
   allProductsArr.forEach((product) =>
-    generateProductMarkup([
+    createNewProduct(
+      true,
       product.ID,
       product.name,
       product.price,
       product.taxes,
       product.ads,
       product.discount,
-      product.category,
       product.totalPrice,
-    ])
+      product.count,
+      product.category
+    )
   );
   productsCounter = allProductsArrFromLocalStorage.length;
   allProductsNum.textContent = `(${productsCounter})`;
@@ -380,15 +346,34 @@ function initEverthingBack() {
 
 btnCreateProduct.addEventListener("click", (event) => {
   event.preventDefault();
-  createNewProduct([
+
+  if (titleInput.value === "")
+    return showError(
+      "Please fill out all required fields (Title, Price, Count)!"
+    );
+  if (priceInput.value === "" || +priceInput.value === 0)
+    return showError(
+      "Please fill out all required fields (Title, Price, Count)!"
+    );
+  if (countInput.value === "" || +countInput.value === 0)
+    return showError(
+      "Please fill out all required fields (Title, Price, Count)!"
+    );
+
+  const randomProductID = Math.floor(Math.random() * 100000000).toString();
+
+  createNewProduct(
+    undefined,
+    randomProductID,
     titleInput.value,
     priceInput.value,
     taxesInput.value,
     adsInput.value,
     discountInput.value,
+    totalPriceSpot.textContent,
     countInput.value,
-    categoryInput.value,
-  ]);
+    categoryInput.value
+  );
 });
 
 searchOptionsSpot.addEventListener("click", (event) => {
@@ -407,8 +392,6 @@ searchInput.addEventListener("input", () => {
   if (searchInput.value === "") return searchForProduct("normal");
   searchForProduct(checkedRadioParent.querySelector("input").value);
 });
-
-window.addEventListener("load", initEverthingBack);
 
 tableBody.addEventListener("click", (event) => {
   const clickedSpot = event.target;
@@ -432,9 +415,9 @@ tableBody.addEventListener("click", (event) => {
     discountInput.value =
       targetProduct.discount !== "0" ? targetProduct.discount : "";
     totalPriceSpot.textContent = targetProduct.totalPrice;
+    countInput.value = targetProduct.count;
     categoryInput.value =
       targetProduct.category !== "No category" ? targetProduct.category : "";
-    countInput.disabled = true;
   }
   if (clickedSpot.classList.contains("btn-delete-product")) {
     manipulateDeletionView("Show");
@@ -447,6 +430,18 @@ tableBody.addEventListener("click", (event) => {
 
 btnUpdateProduct.addEventListener("click", (event) => {
   event.preventDefault();
+  if (titleInput.value === "")
+    return showError(
+      "Please fill out all required fields (Title, Price, Count)!"
+    );
+  if (priceInput.value === "" || +priceInput.value === 0)
+    return showError(
+      "Please fill out all required fields (Title, Price, Count)!"
+    );
+  if (countInput.value === "" || +countInput.value === 0)
+    return showError(
+      "Please fill out all required fields (Title, Price, Count)!"
+    );
   editTheProduct(productID);
 });
 
@@ -472,3 +467,5 @@ document.addEventListener("keydown", (event) => {
   if (event.key === "Escape" && deletionWindow.dataset.state === "visible")
     manipulateDeletionView("Hide");
 });
+
+window.addEventListener("load", initEverthingBack);
